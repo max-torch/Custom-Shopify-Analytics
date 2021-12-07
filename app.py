a245243cfb630027a3b0ca26fc1a6d055572bd79
Alexander Lacson <41433185+max-torch@users.noreply.github.com>
2021-12-08 00:48:20 +0800

    Add Billing Address location Chart

--- app.py
+++ app.py
@@ -18,11 +18,22 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 import requests
+import numpy as np
 
 
 # Global Variables
 RESOURCE = 'orders'
 FIELDS = ['id', 'app_id', 'buyer_accepts_marketing', 'cancel_reason', 'cancelled_at', 'client_details', 'closed_at', 'contact_email', 'created_at', 'current_subtotal_price', 'current_total_discounts', 'current_total_price', 'customer', 'discount_codes', 'email', 'financial_status', 'fulfillment_status', 'gateway', 'landing_site', 'name', 'order_number', 'payment_gateway_names', 'phone', 'processed_at', 'processing_method', 'referring_site', 'source_name', 'subtotal_price', 'total_discounts', 'total_line_items_price', 'total_outstanding', 'total_price', 'updated_at', 'billing_address', 'discount_applications', 'line_items', 'refunds', 'shipping_address']
+
+# DataFrame that contains Philippines zipcodes mapped to their area and province/city
+zipcodes = (
+    pd.read_csv("zipcodes.csv")
+    .rename(columns={"ZIP Code": "ZIP_Code"})
+    .assign(ZIP_Code=lambda x: x.ZIP_Code.astype("object"))
+)
+
+# Dictionary from zipcodes DataFrame for Pandas Series.map() method.
+zipcodesdict = pd.Series(zipcodes["Province or city"].values, index=zipcodes["ZIP_Code"]).to_dict()
 
 
 app = dash.Dash(__name__)
@@ -43,7 +54,7 @@ def measure_time(func):
 @measure_time
 def main():
     orders = get_all_orders()
-    orders = preprocess(orders)
+    orders = preprocess_orders(orders)
     generate_layout(orders)
 
 
@@ -70,19 +81,33 @@ def get_all_orders():
     return(orders)
 
 
-def preprocess(orders):
+def preprocess_orders(orders):
     orders = (
-        orders.drop(columns = ['customer', 'client_details'])
+        orders.drop(columns=['customer', 'client_details'])
         .assign(
-            total_outstanding = pd.to_numeric(orders.total_outstanding),
-            created_at = pd.to_datetime(orders.created_at)
+            total_outstanding=pd.to_numeric(orders.total_outstanding),
+            created_at=pd.to_datetime(orders.created_at)
         )
         .reset_index(drop=True)
     )
     return orders
 
 
-def generate_figures(orders):
+def preprocess_billing_address(billing_address):
+
+    # DataFrame that contains Billing Addresses of each order
+    billing_address = (
+        billing_address.assign(
+            city=lambda x: x.city.str.upper().str.replace("CITY", "").str.strip(),
+            province=lambda x: x.province.replace(r'^\s*$', np.NaN, regex=True),
+            zip=lambda x: pd.to_numeric(x.zip.replace(r"[A-Z, a-z]", np.nan, regex=True).replace('', np.nan)),
+            province_city_fromzip=lambda x: x.zip.map(zipcodesdict).fillna(x.province).fillna(x.city)
+        )
+    )
+    return billing_address
+
+
+def generate_figures(orders, billing_address):
 
     hours_dict = {each : (str(each) + ' AM' if each != 0 else '12 AM') for each in range(12)}
     hours_dict_pm = {each : (str(each - 12) + ' PM' if each != 12 else '12 PM') for each in range(12, 24)}
@@ -113,12 +138,15 @@ def generate_figures(orders):
         12: 'December'
     }
 
+    metro_cities = ['Manila', 'Quezon City', 'Caloocan', 'Las Piñas', 'Makati', 'Malabon', 'Mandaluyong', 'Marikina', 'Muntinlupa', 'Navotas', 'Parañaque', 'Pasay', 'Pasig', 'San Juan', 'Taguig', 'Valenzuela', 'Pateros']
+
+    # This section plots date and time trends.
     def plot_popular_datetimes(datain, xaxisname: str, title: str, map_vals: bool, map_dict=None):
         view = (
             pd.DataFrame(datain)
             .assign(
-                xaxis = lambda x: x.index.map(map_dict).to_list() if map_vals == True else x.index.astype(str),
-                yaxis = lambda x: x[x.columns[0]]
+                xaxis=lambda x: x.index.map(map_dict).to_list() if map_vals == True else x.index.astype(str),
+                yaxis=lambda x: x[x.columns[0]]
             )
             .reset_index(drop=True)
         )
@@ -163,7 +191,20 @@ def generate_figures(orders):
         False
     )
 
-    return fig_hours_day, fig_days_week, fig_days_month, fig_months_year, fig_weeks_year
+    # This section plots location trends.
+    view = (
+        pd.DataFrame(billing_address.province_city_fromzip.replace(r'^\s*$', np.NaN, regex=True).value_counts())
+        .reset_index(drop=False)
+        .rename(columns={'index': 'xaxis', 'province_city_fromzip': 'yaxis'})
+        .assign(Province_or_City = lambda x: x.xaxis.apply(lambda y: 'Metro Manila' if y in metro_cities else y))
+    )
+
+    xaxisname = "Province or City"
+    title = "Distribution of Orders over Billing Address Location"
+    fig_location = px.bar(view, x='Province_or_City', y='yaxis', labels={'xaxis': xaxisname, 'yaxis': 'Quantity of Orders'}, title=title, hover_data=["xaxis"])
+    fig_location.update_layout(barmode='stack', xaxis={'categoryorder':'total descending'})
+
+    return fig_hours_day, fig_days_week, fig_days_month, fig_months_year, fig_weeks_year, fig_location
 
 
 def generate_layout(orders):
@@ -209,6 +250,10 @@ def generate_layout(orders):
         html.Div(children=[
             dcc.Graph(id='graph5'),
         ]),
+        html.Div(children=[
+            dcc.Graph(id='graph6'),
+        ]),
+
     ])
 
     @app.callback(
@@ -217,7 +262,8 @@ def generate_layout(orders):
             Output('graph2', 'figure'),
             Output('graph3', 'figure'),
             Output('graph4', 'figure'),
-            Output('graph5', 'figure')
+            Output('graph5', 'figure'),
+            Output('graph6', 'figure')
         ],
         inputs=[
             Input('date-picker', 'start_date'),
@@ -226,7 +272,9 @@ def generate_layout(orders):
     )
     def update_figures(start_date, end_date):
         date_filtered_orders = orders[orders.created_at.between(start_date, end_date)]
-        return generate_figures(date_filtered_orders)
+        billing_address = pd.json_normalize(date_filtered_orders[date_filtered_orders["billing_address"].notnull()]["billing_address"])
+        billing_address = preprocess_billing_address(billing_address)
+        return generate_figures(date_filtered_orders, billing_address)
 
 
 if __name__ == "__main__":
